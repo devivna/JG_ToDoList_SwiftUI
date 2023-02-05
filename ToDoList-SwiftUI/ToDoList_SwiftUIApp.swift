6b600a97d2c38cfbdb846a65165d97cd0d793549
Nadia <ivashchenko.nadia@gmail.com>
2023-02-05 21:54:53 +0200

    Initial Commit

---
+++ ToDoList-SwiftUI/ToDoList_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_SwiftUIApp.swift
+//  ToDoList-SwiftUI
+//
+//  Created by Students on 05.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
