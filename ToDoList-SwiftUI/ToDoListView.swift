60a6305cdbb9861035974fc472c72e67f3c522ca
Nadia <ivashchenko.nadia@gmail.com>
2023-02-06 13:56:27 +0200

    5.5.1 delete NavStack in Main View and add it to .sheet modifier

--- ToDoList-SwiftUI/ToDoListView.swift
+++ ToDoList-SwiftUI/ToDoListView.swift
@@ -13,33 +13,33 @@ struct ToDoListView: View {
         "Learn Swift", "Build Apps", "Change the World", "Do my best"]
     
     @State private var sheetIsPresented = false
-
+    
     var body: some View {
-        NavigationStack {
-            VStack {
-                List{
-                    ForEach(toDos, id: \.self) { item in
-                        NavigationLink(item) {
-                            DetailView(passedValue: item)
-                        }
-                        .font(.title3)
+        VStack {
+            List{
+                ForEach(toDos, id: \.self) { item in
+                    NavigationLink(item) {
+                        DetailView(passedValue: item)
                     }
+                    .font(.title3)
                 }
             }
-            .navigationTitle("ToDo List")
-            .navigationBarTitleDisplayMode(.large)
-            .listStyle(.insetGrouped)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button {
-                        sheetIsPresented.toggle()
-                    } label: {
-                        Image(systemName: "plus")
-                    }
+        }
+        .navigationTitle("ToDo List")
+        .navigationBarTitleDisplayMode(.large)
+        .listStyle(.insetGrouped)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button {
+                    sheetIsPresented.toggle()
+                } label: {
+                    Image(systemName: "plus")
                 }
             }
-            
-            .fullScreenCover(isPresented: $sheetIsPresented) {
+        }
+        
+        .fullScreenCover(isPresented: $sheetIsPresented) {
+            NavigationStack {
                 DetailView(passedValue: "")
             }
         }
